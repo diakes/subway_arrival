73f0374c24b56e5435727e4005eefcfa221d4c8a
3sooham <50095515+3sooham@users.noreply.github.com>
2021-06-06 23:53:16 +0900

    0606 안되던 것

---
+++ Mk2/src/main/java/com/example/accessingdatamysql/Mk2Application.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mk2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mk2Application.class, args);
+	}
+
+}
