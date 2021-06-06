31a348b38a0717412cecb36a05da9504195e9647
diakes <diakes@naver.com>
2021-06-06 23:48:32 +0900

    add simpleMember

---
+++ simpleMember/src/test/java/com/dong/simpleMember/SimpleMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dong.simpleMember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
